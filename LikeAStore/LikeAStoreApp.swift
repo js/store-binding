3d253b1594faeb29033882cdabfe201d6c945fc1
Johan Sørensen <johan.sorensen@mentra.co>
2023-04-21 07:44:51 +0200

    Initial Commit

---
+++ LikeAStore/LikeAStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  LikeAStoreApp.swift
+//  LikeAStore
+//
+//  Created by Johan Sørensen on 21/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct LikeAStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
